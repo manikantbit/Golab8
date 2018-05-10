76cb23e232cded4b4ec0de1e2f08d4dcfcc93794
manikantbit <manikant_bit@rediffmail.com>
2018-05-10 13:25:24 -0700

    UI change

--- cashapp client/src/components/Navbar.js
+++ cashapp client/src/components/Navbar.js
@@ -10,15 +10,15 @@ class Navbar extends Component {
         return (
             <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
 
-  <a className="navbar-brand" href="http://localhost:3001"><img src={Image} height ='60' width='130'></img></a>
+  <a className="navbar-brand" href="https://team-project281.herokuapp.com"><img src={Image} height ='60' width='130'></img></a>
 
   
   <ul className=" nav navbar-nav navbar-right">
     <li className="nav-item ">
-    <a className="nav-link " href="http://localhost:3001/login">Login</a>
+    <a className="nav-link " href="https://team-project281.herokuapp.com/login">Login</a>
     </li>
     <li className="nav-item">
-      <a className="nav-link" href="http://localhost:3001/signup">Signup</a>
+      <a className="nav-link" href="https://team-project281.herokuapp.com/signup">Signup</a>
     </li>
    
     
